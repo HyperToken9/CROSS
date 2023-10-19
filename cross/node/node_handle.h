98d066d645dbbbdabcf741c1e66e4a0c3755b247
Nathan Adrian Saldanha <nathanadrian2002@gmail.com>
2023-10-20 04:26:15 +0530

    reviewing peer to peer

--- cross/node/node_handle.h
+++ cross/node/node_handle.h
@@ -2,9 +2,24 @@
 
 #include <netinet/in.h>
 
+struct Subscription{
+    
+    struct Subscription * next;
+};
+
+typedef struct Subscription Subscription;
+
+struct Publication{
+    
+    struct Publication * next;
+};
+
+typedef struct Publication Publication;
+
+
 struct NodeHandle{
 
-    /* Networking */
+    /* Networking with Master*/
     int socket_descriptor;
     struct sockaddr_in address;
 
@@ -12,6 +27,8 @@ struct NodeHandle{
     int is_registered;
     char node_name[50];
 
+    Subscription * subscriptions;
+    Publication * publications;
 };
 
 typedef struct NodeHandle NodeHandle;
